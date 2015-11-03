9f52ede97832e69cb3d2de36253deef6761f4e37
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-11-03 15:49:06 -0200

    Add sprite animations

--- scripts/entities/sprite.js
+++ scripts/entities/sprite.js
@@ -1,13 +1,26 @@
 import Entity from 'entities/entity';
 
 export default class Sprite extends Entity {
-  constructor(image, x, y, width=image.width, height=image.height) {
+  constructor(image, x, y, width=image.width, height=image.height, clips=[], frequency=10) {
     super(image, x, y, width, height);
+
+    this.clock = 1;
+    this.index = 0;
+    this.clips = clips;
+    this.indexes = clips.map((o, i) => i);
+    this.frequency = frequency;
   }
 
   draw(context) {
+    if ((this.clock++ % this.frequency) === 0) {
+      this.index = (this.index + 1) % this.indexes.length;
+    }
+
     let points = this.polygon.points;
-    context.drawImage(image, points[0].x - this.camera.x,
-                             points[0].y - this.camera.y);
+    let clip = this.clips[this.indexes[this.index]];
+    context.drawImage(this.image, ...clip,
+      points[0].x - this.camera.x, points[0].y - this.camera.y,
+      this.box.width, this.box.height
+    );
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,31 @@ import imageLoad from 'image-load';
 import Camera from 'camera';
 import Spawner from 'spawner';
 import StaticEntity from 'entities/static';
+import Sprite from 'entities/sprite';
 
-imageLoad([require('../assets/images/platform.png')], function(platform) {
+imageLoad([
+    require('../assets/images/platform.png'),
+    require('../assets/images/woman.png'),
+    require('../assets/images/man.png'),
+  ], function(platform, woman, man) {
   let canvas = document.getElementById('canvas');
   let context = canvas.getContext('2d');
   window.camera = new Camera(canvas);
   window.camera.speed.x = 0.2;
+
+  let settings = [
+    (canvas.width / 3) - 16, (canvas.height / 2) - 16, 32, 32,
+    [
+      [0, 64, 32, 32],
+      [32, 64, 32, 32],
+      [64, 64, 32, 32],
+    ], 10
+  ];
+
+  woman = new Sprite(woman, ...settings);
+  woman.polygon.speed.x = 0.2;
+  woman.indexes = [0, 1, 2, 1];
+  man = new Sprite(man, ...settings);
 
   let spawner = new Spawner(
     [new StaticEntity(platform)]
@@ -23,6 +42,8 @@ imageLoad([require('../assets/images/platform.png')], function(platform) {
     for (let i = 0; i < platforms.length; ++i) {
       platforms[i].render(context);
     }
+
+    woman.render(context);
 
     requestAnimationFrame(loop);
   }
